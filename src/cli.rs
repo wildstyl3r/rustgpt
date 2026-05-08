4ff5a9330641b256a5255bdff0f952f0fbfe5170
Mike <wildstyl3r@yandex.ru>
2026-05-08 10:37:28 +0300

    refactor: move cli structures to a separate file, move model parameters into a substructure in TrainArgs

---
+++ src/cli.rs
@@ -0,0 +1,69 @@
+use clap::{Args, Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser, Debug, Serialize, Deserialize)]
+#[command(author, version, about)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub mode: Mode,
+}
+
+#[derive(Subcommand, Debug, Serialize, Deserialize)]
+pub enum Mode {
+    Train {
+        #[command(subcommand)]
+        config: Config,
+    },
+    Eval {
+        #[arg(long)]
+        checkpoint: std::path::PathBuf,
+    },
+}
+
+#[derive(Subcommand, Debug, Serialize, Deserialize)]
+pub enum Config {
+    File {
+        #[arg(long)]
+        path: std::path::PathBuf,
+    },
+    Cli(TrainArgs),
+}
+
+#[derive(Args, Debug, Serialize, Deserialize)]
+pub struct TrainArgs {
+    #[arg(long, default_value_t = 3e-3)]
+    pub learning_rate: f64,
+
+    #[arg(long, default_value_t = 15001)]
+    pub max_iters: i64,
+
+    #[arg(long, default_value_t = 500)]
+    pub eval_interval: i64,
+
+    #[arg(long, default_value_t = 200)]
+    pub eval_iters: i64,
+
+    #[arg(long, default_value_t = 32)]
+    pub batch_size: i64,
+
+    #[arg(long, default_value = "input.txt")]
+    pub input_path: String,
+
+    #[command(flatten)]
+    pub model: ModelArgs,
+}
+
+#[derive(Args, Debug, Serialize, Deserialize)]
+pub struct ModelArgs {
+    #[arg(long, default_value_t = 0.2)]
+    pub dropout: f64,
+
+    #[arg(long, default_value_t = 8)]
+    pub block_size: i64,
+
+    #[arg(long, default_value_t = 4)]
+    pub n_blocks: i64,
+
+    #[arg(long, default_value_t = 32)]
+    pub n_embed: i64,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,33 @@
 use std::fs::{self, File};
 use std::io::{BufReader, BufWriter};
 
-use clap::{Args, Parser, Subcommand};
-use serde::{Deserialize, Serialize};
+use clap::Parser;
 use tch::{nn, nn::OptimizerConfig, IndexOp, Tensor};
 
 mod baseline;
+mod cli;
 mod language_model;
 mod tokenizer;
 mod utils;
 
 use crate::baseline::model::TransformerLanguageModel;
+use crate::cli::{Cli, Config, Mode, TrainArgs};
 use crate::language_model::LanguageModel;
 use crate::tokenizer::{Token, Tokenizer};
 use crate::utils::{estimate_loss, get_batch};
-
-#[derive(Parser, Debug, Serialize, Deserialize)]
-#[command(author, version, about)]
-struct Cli {
-    #[command(subcommand)]
-    mode: Mode,
-}
-
-#[derive(Subcommand, Debug, Serialize, Deserialize)]
-enum Mode {
-    Train {
-        #[command(subcommand)]
-        config: TrainConfig,
-    },
-    Eval {
-        #[arg(long)]
-        checkpoint: std::path::PathBuf,
-    },
-}
-
-#[derive(Subcommand, Debug, Serialize, Deserialize)]
-pub enum TrainConfig {
-    File {
-        #[arg(long)]
-        path: std::path::PathBuf,
-    },
-    Cli(TrainArgs),
-}
-
-#[derive(Args, Debug, Serialize, Deserialize)]
-pub struct TrainArgs {
-    #[arg(long, default_value_t = 0.2)]
-    pub dropout: f64,
-
-    #[arg(long, default_value_t = 3e-3)]
-    pub learning_rate: f64,
-
-    #[arg(long, default_value_t = 15001)]
-    pub max_iters: i64,
-
-    #[arg(long, default_value_t = 500)]
-    pub eval_interval: i64,
-
-    #[arg(long, default_value_t = 200)]
-    pub eval_iters: i64,
-
-    #[arg(long, default_value_t = 32)]
-    pub batch_size: i64,
-
-    #[arg(long, default_value_t = 8)]
-    pub block_size: i64,
-
-    #[arg(long, default_value_t = 4)]
-    pub n_blocks: i64,
-
-    #[arg(long, default_value_t = 32)]
-    pub n_embed: i64,
-
-    #[arg(long, default_value = "input.txt")]
-    pub input_path: String,
-}
 
 fn main() {
     let cli = Cli::parse();
     let git_hash = env!("GIT_HASH");
 
-    let (tokenizer, model, params) = match cli.mode {
+    let (tokenizer, model) = match cli.mode {
         Mode::Train { config } => {
             let params = match config {
-                TrainConfig::File { path } => {
+                Config::File { path } => {
                     let content = std::fs::read_to_string(path).expect("Read error");
                     toml::from_str(&content).expect("TOML error")
                 }
-                TrainConfig::Cli(p) => p,
+                Config::Cli(p) => p,
             };
             tch::manual_seed(1337);
 
@@ -106,10 +46,10 @@ fn main() {
             let model = TransformerLanguageModel::new(
                 vs.root(),
                 tokenizer.vocabulary.len().try_into().unwrap(),
-                params.n_embed,
-                params.block_size,
-                params.n_blocks,
-                params.dropout,
+                params.model.n_embed,
+                params.model.block_size,
+                params.model.n_blocks,
+                params.model.dropout,
             );
 
             let mut optimizer = nn::AdamW::default()
@@ -123,7 +63,7 @@ fn main() {
                         &train_data,
                         &validation_data,
                         params.batch_size,
-                        params.block_size,
+                        params.model.block_size,
                         &model,
                     );
                     println!(
@@ -132,7 +72,7 @@ fn main() {
                     );
                 }
 
-                let (xb, yb) = get_batch(&train_data, params.batch_size, params.block_size);
+                let (xb, yb) = get_batch(&train_data, params.batch_size, params.model.block_size);
 
                 let (loss, _) = model.forward_with_loss(&xb, &yb, true);
                 optimizer.zero_grad();
@@ -149,7 +89,7 @@ fn main() {
             vs.save(log_dir.join("model.safetensors")).unwrap();
             let tk_file = File::create(log_dir.join("tokenizer.json")).unwrap();
             serde_json::to_writer(BufWriter::new(tk_file), &tokenizer).unwrap();
-            (tokenizer, model, params)
+            (tokenizer, model)
         }
         Mode::Eval { checkpoint } => {
             let tk_file =
@@ -165,17 +105,17 @@ fn main() {
             let model = TransformerLanguageModel::new(
                 vs.root(),
                 tokenizer.vocabulary.len().try_into().unwrap(),
-                params.n_embed,
-                params.block_size,
-                params.n_blocks,
-                params.dropout,
+                params.model.n_embed,
+                params.model.block_size,
+                params.model.n_blocks,
+                params.model.dropout,
             );
             vs.load(checkpoint.join("model.safetensors")).unwrap();
-            (tokenizer, model, params)
+            (tokenizer, model)
         }
     };
 
-    let idx = Tensor::zeros([1, params.batch_size], (tch::Kind::Int, tch::Device::Cpu));
+    let idx = Tensor::zeros([1, 1], (tch::Kind::Int, tch::Device::Cpu));
     println!(
         "[{}]",
         tokenizer.decode(&Vec::<Token>::try_from(model.generate(idx, 500).i(0)).unwrap())
